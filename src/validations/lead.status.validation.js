253d4c265970966e8bb3ed3232887cd20d619f75
adithyakrishnapn <adithyakrishnapn@protonmail.com>
2026-07-14 15:26:13 +0530

    validation for lead customer completed

--- src/validations/lead.status.validation.js
+++ src/validations/lead.status.validation.js
@@ -1,9 +1,17 @@
-const leadToCustomerValidation = (req, res, next) => {
-    const status = req.body.status;
-    if (status === 'converted') {
-        return res.status(400).json({ error: 'Cant be modified because it is already converted' });
+import Lead from '../models/lead.model.js';
+const leadToCustomerValidation = async(req, res, next) => {
+    const {id} = req.params;
+    const leadData = await Lead.findById(id);
+    if(!leadData){
+        return res.status(404).json({message: 'Lead not found'});
+    }
+
+    const status = leadData.status;
+    if(status === 'converted'){
+        return res.status(400).json({message: 'Lead is already converted to customer'});
     }
     next();
+
 };
 
 export default leadToCustomerValidation;
